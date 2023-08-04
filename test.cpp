334ddc38c368f8411341ab938bb090c90f898e4c
Quan Doan <quandoan@Quans-MacBook-Pro.local>
2023-08-03 21:34:38 -0400

    Apply padding and remove padding

--- test.cpp
+++ test.cpp
@@ -1,7 +1,15 @@
 #include "key_rotation.h"
+#include "triple_des.h"
 
 int main(){
     string key = "0001001100110100010101110111100110011011101111001101111111110001";
+    //Test padding
+    TripleDES object0 = TripleDES(key, "0");
+    string input = "1111";
+    string padding = object0.encrypt(input);
+    string noPadding = object0.decrypt(padding);
+    assert(noPadding == input);
+    //Test key roataion
     keyRotation object = keyRotation(key);
     for (int i = 0; i < 16; i++){
         key = object.nextRound();
--- triple_des.h
+++ triple_des.h
@@ -3,20 +3,21 @@
 #define TRIPLE_DES_H
 
 #include <string>
+#include <math.h>
 using namespace std;
 class TripleDES {
 public:
     // Constructor for 2-Key Triple DES (16-byte key).
-    TripleDES(const string& key);
+    TripleDES(const string& key1, const string& key2);
 
     // Constructor for 3-Key Triple DES (24-byte key).
     TripleDES(const string& key1, const string& key2, const string& key3);
 
     // Encrypts the plaintext using Triple DES.
-    string encrypt(const string& plaintext);
+    string encrypt(string& plaintext);
 
     // Decrypts the ciphertext using Triple DES.
-    string decrypt(const string& ciphertext);
+    string decrypt(string& ciphertext);
 
 private:
     string key1_; // First 8 bytes of the 2-key or 3-key Triple DES key.
@@ -28,11 +29,82 @@ private:
     //once the other parts are completed
     string des_encrypt(const string& input, const string& subkey);
     string des_decrypt(const string& input, const string& subkey);
-    string apply_padding(const string& input);
-    string remove_padding(const string& input);
+    string apply_padding(string& input);
+    string remove_padding(string& input);
 
     // XOR two strings of equal length.
     string xor_strings(const string& str1, const string& str2);
+    //To binary
+    string toBinary(int num);
+    int toDecimal(string binary);
 };
 
 #endif // TRIPLE_DES_H
+TripleDES::TripleDES(const string& key1, const string& key2){
+    key1_ = key1;
+    key2_ = key2;
+    key3_ = "";
+}
+TripleDES::TripleDES(const string& key1, const string& key2, const string& key3){
+    key1_ = key1;
+    key2_ = key2;
+    key3_ = key3;
+}
+string TripleDES::encrypt(string& plaintext){
+    //In construction
+    return apply_padding(plaintext);
+}
+string TripleDES::decrypt(string& ciphertext){
+    //In construction
+    return remove_padding(ciphertext);
+}
+string TripleDES::toBinary(int num){
+    string binary = "";
+    int comparer;
+    for (int i = 7; i >= 0; i--){
+        comparer = num - pow(2,i);
+        if (comparer >= 0){
+            binary = binary + "1";
+            num = num - pow(2,i);
+        }
+        else{
+            binary = binary + "0";
+        }
+    }
+    return binary;
+}
+int TripleDES::toDecimal(string binary){
+        int num = 0;
+        for (int i = 0; i < 8; i++){
+            if (binary[i] == '1'){
+                num = num + pow(2, i);
+            }
+        }
+        return num;
+}
+string TripleDES::apply_padding(string& input){
+    int length = input.length();
+    string output = input;
+    int paddingBits;
+    if (length % 64 != 0){
+        paddingBits = 64 - (length % 64);
+    }
+    for (int i = 0; i < paddingBits - 8; i++){
+        output = output + "0";
+    }
+    output = output + toBinary(paddingBits);
+    return output;
+}
+string TripleDES::remove_padding(string& input){
+    string lastByte = "";
+    string output = "";
+    int length = input.length();
+    for (int i = 0; i < 8; i++){
+        lastByte = lastByte + input[length - 8 + i];
+    }
+    int paddingBits = toDecimal(lastByte);
+    for (int i = 0; i < (length - paddingBits); i++){
+        output = output + input[i];
+    }
+    return output;
+}
